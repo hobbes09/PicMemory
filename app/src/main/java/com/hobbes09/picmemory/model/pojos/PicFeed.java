25cddc07cb0f79df2bc06acea816d67d57ad3014
hobbes09 <sourin.sutradhar@gmail.com>
2017-05-08 16:47:37 +0530

    pojos

---
+++ app/src/main/java/com/hobbes09/picmemory/model/pojos/PicFeed.java
@@ -0,0 +1,48 @@
+package com.hobbes09.picmemory.model.pojos;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by hobbes09 on 08/05/17.
+ */
+
+public class PicFeed {
+
+    @SerializedName("Search")
+    @Expose
+    private List<Search> search = null;
+    @SerializedName("totalResults")
+    @Expose
+    private String totalResults;
+    @SerializedName("Response")
+    @Expose
+    private String response;
+
+    public List<Search> getSearch() {
+        return search;
+    }
+
+    public void setSearch(List<Search> search) {
+        this.search = search;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+}
---
+++ app/src/main/java/com/hobbes09/picmemory/utils/GlobalConfig.java
@@ -0,0 +1,11 @@
+package com.hobbes09.picmemory.utils;
+
+/**
+ * Created by hobbes09 on 08/05/17.
+ */
+
+public class GlobalConfig {
+
+    public static final String BASE_API = "http://www.omdbapi.com";
+
+}
